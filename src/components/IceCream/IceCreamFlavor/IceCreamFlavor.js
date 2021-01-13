c5ebc70880edada70d51c2513ad3202e8954c551
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-01-13 20:14:12 +0530

    PropTypes commit

--- src/components/IceCream/IceCreamFlavor/IceCreamFlavor.js
+++ src/components/IceCream/IceCreamFlavor/IceCreamFlavor.js
@@ -1,44 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './IceCreamFlavor.css';
+import PropTypes from 'prop-types';
 
-const iceCreamFlavor = props => {
-  let flavor = null;
+class IceCreamFlavor extends Component {
+  render() {
+    let flavor = null;
 
-  switch (props.type) {
-    case ('bread-bottom'):
-      flavor = <div className={classes.BreadBottom}></div>;
-      break;
+    switch (this.props.type) {
+      case ('bread-bottom'):
+        flavor = <div className={classes.BreadBottom}></div>;
+        break;
 
-    case ('bread-top'):
-      flavor = (
-        <div className={classes.BreadTop}>
-          <div className={classes.Seeds1}></div>
-          <div className={classes.Seeds2}></div>
-        </div>
-      );
-      break;
+      case ('bread-top'):
+        flavor = (
+          <div className={classes.BreadTop}>
+            <div className={classes.Seeds1}></div>
+            <div className={classes.Seeds2}></div>
+          </div>
+        );
+        break;
 
-    case ('meat'):
-      flavor = <div className={classes.Meat}></div>;
-      break;
+      case ('meat'):
+        flavor = <div className={classes.Meat}></div>;
+        break;
 
-    case ('cheese'):
-      flavor = <div className={classes.Cheese}></div>;
-      break;
+      case ('cheese'):
+        flavor = <div className={classes.Cheese}></div>;
+        break;
 
-    case ('bacon'):
-      flavor = <div className={classes.Bacon}></div>;
-      break;
+      case ('bacon'):
+        flavor = <div className={classes.Bacon}></div>;
+        break;
 
-    case ('salad'):
-      flavor = <div className={classes.Salad}></div>;
-      break;
-    
-    default:
-      flavor = null;
+      case ('salad'):
+        flavor = <div className={classes.Salad}></div>;
+        break;
+      
+      default:
+        flavor = null;
+    }
+
+    return flavor;
   }
-
-  return flavor;
 }
 
-export default iceCreamFlavor;
+IceCreamFlavor.PropTypes = {
+  type: PropTypes.string.isRequired
+}
+
+export default IceCreamFlavor;
