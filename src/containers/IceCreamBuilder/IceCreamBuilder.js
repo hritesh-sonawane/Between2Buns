6752b6e56b8cf16f9dc3939a5c61e86640be1d15
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-01-15 21:48:39 +0530

    set state to add flavors

--- src/containers/IceCreamBuilder/IceCreamBuilder.js
+++ src/containers/IceCreamBuilder/IceCreamBuilder.js
@@ -3,6 +3,13 @@ import Aux from '../../hoc/Auxiliary';
 import IceCream from '../../components/IceCream/IceCream';
 import BuildControls from '../../components/IceCream/BuildControls/BuildControls';
 
+const FLAVOR_PRICES = {
+  grape: 20,
+  unicorn: 20,
+  blackcurrent: 15,
+  strawberry: 15
+}
+
 class IceCreamBuilder extends Component {
   // constructor(props) {
   //   super(props);
@@ -15,13 +22,37 @@ class IceCreamBuilder extends Component {
       unicorn: 1,
       blackcurrent: 1,
       strawberry: 1
-    }
+    },
+    totalPrice: 30
   }
+
+  addFlavorHandler = type => {
+    const oldCount = this.state.flavors[type];
+    const updatedCount = oldCount + 1;
+    const updatedFlavors = {
+        ...this.state.flavors
+    }
+    updatedFlavors[type] = updatedCount;
+    const priceAddition = FLAVOR_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice= oldPrice + priceAddition;
+    this.setState({
+      totalPrice: newPrice,
+      flavors: updatedFlavors
+    })
+  }
+
+  removeFlavorHandler = type => {
+
+  }
+
   render() {
     return (
       <Aux>
         <IceCream flavors={this.state.flavors} />
-        <BuildControls />
+        <BuildControls
+          flavorAdded={this.addFlavorHandler}
+        />
       </Aux>
     );
   }
