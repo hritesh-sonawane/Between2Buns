ffe404a8d76e9e560937e66548ffe3c42eef2011
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-01-26 20:39:38 +0530

    remove old interceptors

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -9,15 +9,21 @@ const withErrorHandler = (WrappedComponent, axios) => {
     state = {
       error: null
     }
-
+    // hook not recommended
     componentWillMount() {
-      axios.interceptors.request.use(req => {
+      this.reqInterceptor = axios.interceptors.request.use(req => {
         this.setState({error: null});
         return req;
-      })
-      axios.interceptors.response.use(res => res, error => {
+      });
+      this.resInterceptor = axios.interceptors.response.use(res => res, error => {
         this.setState({error: error});
       });
+    }
+
+    // to prevent memory leaks, mite become dead interceptors!
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.response.eject(this.resInterceptor);
     }
 
     errorConfirmedHandler = () => {
