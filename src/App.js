425121d6ee160d51f6c8882c0b5e1d7f93c4fd73
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-02-05 22:15:14 +0530

    setting up routing & routes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import IceCreamBuilder from './containers/IceCreamBuilder/IceCreamBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import { Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <Layout>
-         <IceCreamBuilder />
-         <Checkout />
-       </Layout>
+        <Layout>
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/" exact component={IceCreamBuilder} />
+            </Switch>
+        </Layout>
       </div>
     );
   }
