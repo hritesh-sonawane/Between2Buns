c16ce6e4929a626b88594ba16640c5e72432bdfe
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-02-08 16:33:14 +0530

    implementing NavLink

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -4,7 +4,7 @@ import classes from './Order.css';
 const order = props => (
   <div className={classes.Order}>
     <p>Flavors: Unicorn (1)</p>
-    <p>Price: <strong>RUP 120</strong></p>
+    <p>Price: <strong>₹120</strong></p>
   </div>
 );
 
