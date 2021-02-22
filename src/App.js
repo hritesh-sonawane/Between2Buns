31bb14dc8abf1dc22cd3de42d917e026e75bb8a9
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-02-22 15:29:08 +0530

    App.js review

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import Layout from './hoc/Layout/Layout';
+import Layout from './hoc/Layout/Layout';   // hoc
 import IceCreamBuilder from './containers/IceCreamBuilder/IceCreamBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import { Route, Switch } from 'react-router-dom';
 import Orders from './containers/Orders/Orders';
 
 // The structure/architecture of the application
+// Routing is done in App.js?
+// Layout is an HOC
+// Checkout | Orders | IceCreamBuilder -> are containers
 class App extends Component {
   render() {
     return (
