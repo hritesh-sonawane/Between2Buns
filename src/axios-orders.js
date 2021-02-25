4ec460b80e22f64c7619ea22f26d441bb1dce5c2
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-02-25 21:45:44 +0530

    axios-orders review

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+// baseurl defines absolute path depending on your server. 
+// You need not to change it manually if the server changes.
+
 const insance = axios.create({
   baseURL: 'https://what-the-scoop-default-rtdb.firebaseio.com/'
 });
