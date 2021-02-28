0f7672a45e9bd5c0c049b8bd6ecb9a6d558e15f5
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-02-28 18:55:27 +0530

    modal review

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -6,10 +6,14 @@ import Backdrop from '../Backdrop/Backdrop';
 // if modal is shown, backdrop is shown
 class Modal extends Component {
 
+  // The shouldComponentUpdate method allows us to exit the complex react update life cycle to avoid calling it again and again on every re-render
+  // It only updates the component if the props passed to it changes
   shouldComponentUpdate(nextProps, nextState) {
     return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
   }   // actually not required
 
+  // componentDidUpdate: called when a component got updated
+  // This might happen if new props have been provided by a parent component or an internal state has been changed
   componentDidUpdate() {
     console.log('[Modal] DidUpdate');
   }
