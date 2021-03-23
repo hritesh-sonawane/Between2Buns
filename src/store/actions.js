d1cab956529d3145000ca641aab5cf50b0d3299f
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-03-23 20:31:11 +0530

    add redux react-redux

---
+++ src/store/actions.js
@@ -0,0 +1,2 @@
+export const ADD_FLAVOR = "ADD_FLAVOR";
+export const REMOVE_FLAVOR = "REMOVE_FLAVOR";
---
+++ src/store/reducer.js
@@ -0,0 +1,10 @@
+import * as actionTypes from "./actions";
+
+const initialState = {
+  flavors: null,
+  totalPrice: 20,
+};
+
+const reducer = (state = initialState, action) => {};
+
+export default reducer;
